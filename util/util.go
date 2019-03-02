2421eac434fb46514db4cb3bb80bae54fe1059eb
Franck <snake386@hotmail.com>
2019-03-02 17:03:51 -0500

    added GetCurrentPath()

--- util/util.go
+++ util/util.go
@@ -1,10 +1,19 @@
 package util
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func CheckError(err error, context string) {
 	if err != nil {
 		fmt.Println(fmt.Sprintf("Context: %s.", context))
 		fmt.Println(err)
 	}
+}
+
+func GetCurrentPath() string {
+	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	return dir
 }
