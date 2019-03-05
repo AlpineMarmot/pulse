3800c33c41a2d5c5a3e9d96222912b66a2a6444f
Franck <snake386@hotmail.com>
2019-03-04 21:22:19 -0500

    added a generic html content middleware

---
+++ middleware/html_attribute.go
@@ -0,0 +1,40 @@
+package middleware
+
+import (
+	"github.com/gocolly/colly"
+	"go.mongodb.org/mongo-driver/bson"
+	"pulse/database"
+	"time"
+)
+
+type HtmlAttributeDefinition struct {
+	Collection  string `yaml:"collection"`
+	Selector    string `yaml:"selector"`
+	Tag         string `yaml:"tag"`
+	Attr        string `yaml:"attr"`
+	ContextAttr string `yaml:"context-attr"`
+}
+
+func GetHtmlAttributeString(htmlAttribute HtmlAttributeDefinition) string {
+	if len(htmlAttribute.Selector) > 0 {
+		return htmlAttribute.Selector
+	}
+	return htmlAttribute.Tag + "[" + htmlAttribute.Attr + "]"
+}
+
+func HtmlAttribute(db database.MongoDb, htmlAttribute HtmlAttributeDefinition) func(e *colly.HTMLElement) {
+	return func(e *colly.HTMLElement) {
+		coll := db.Collection(htmlAttribute.Collection)
+		value := e.Attr(htmlAttribute.Attr)
+		context := ""
+		if len(htmlAttribute.ContextAttr) > 0 {
+			context = e.Attr(htmlAttribute.ContextAttr)
+		}
+		_, _ = coll.InsertOne(db.GetQueryContext(), bson.M{
+			"dt_created": time.Now(),
+			"selector":   GetHtmlAttributeString(htmlAttribute),
+			"context":    context,
+			"value":      value,
+		})
+	}
+}
