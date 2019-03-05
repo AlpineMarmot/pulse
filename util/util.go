2d8f1fa76c9272849a3a4b5cde86ca3ab961662f
Franck <snake386@hotmail.com>
2019-03-04 21:21:24 -0500

    added FatalError()

--- util/util.go
+++ util/util.go
@@ -13,6 +13,14 @@ func CheckError(err error, context string) {
 	}
 }
 
+func FatalError(msg string, err error) {
+	if err != nil {
+		fmt.Println(msg, ":")
+		fmt.Println("\t", err.Error())
+		os.Exit(1)
+	}
+}
+
 func GetCurrentPath() string {
 	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
 	return dir
