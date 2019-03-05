34079ca595a977ac2422e20470753ca3234f3828
Franck <snake386@hotmail.com>
2019-03-04 21:20:41 -0500

    updated config structure

--- config/config.go
+++ config/config.go
@@ -4,70 +4,86 @@ import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
+	"pulse/middleware"
 	"pulse/util"
 	"time"
 )
 
 type Config struct {
-	Mongo struct {
-		Address  string
-		Database string
-	}
-	Crawler struct {
-		AllowedDomains    []string `yaml:"allowed-domains"`
-		AllowURLRevisit   bool     `yaml:"allow-url-revisit"`
-		Async             bool     `yaml:"async"`
-		CheckHead         bool     `yaml:"check-head"`
-		DetectCharset     bool     `yaml:"detect-charset"`
-		DisallowedDomains []string `yaml:"disallowed-domains"`
-		IgnoreRobotsTxt   bool     `yaml:"ignore-robots-txt"`
-		Limit             struct {
-			DomainRegexp string        `yaml:"domain-regexp"`
-			DomainGlob   string        `yaml:"domain-glob"`
-			Delay        time.Duration `yaml:"delay"`
-			RandomDelay  time.Duration `yaml:"random-delay"`
-			Parallelism  int           `yaml:"parallelism"`
-		}
-		MaxBodySize         int           `yaml:"max-body-size"`
-		MaxDepth            int           `yaml:"max-depth"`
-		MaxUrlVisited       int64         `yaml:"max-url-visited"`
-		RandomUserAgents    bool          `yaml:"random-user-agents"`
-		SleepBetweenRequest time.Duration `yaml:"sleep-between-request"`
-		Storage             struct {
-			ClearOnStart bool `yaml:"clear-on-start"`
-			Redis        struct {
-				Address  string
-				Password string
-				Db       int
-				Prefix   string
-			}
-			MongoDb struct {
-				Database string
-				Uri      string
-			}
-		}
-		Proxy      []string `yaml:"proxy"`
-		UserAgents []string `yaml:"user-agents"`
-	}
+	Pulse   configPulse   `yaml:"pulse"`
+	Crawler configCrawler `yaml:"crawler"`
+}
+
+type configPulse struct {
+	Mongo       configMongoDb                        `yaml:"mongo"`
+	Middlewares []string                             `yaml:"middlewares"`
+	Html        []middleware.HtmlAttributeDefinition `yaml:"html"`
+}
+
+type configCrawler struct {
+	AllowedDomains      []string             `yaml:"allowed-domains"`
+	AllowURLRevisit     bool                 `yaml:"allow-url-revisit"`
+	Async               bool                 `yaml:"async"`
+	CheckHead           bool                 `yaml:"check-head"`
+	DetectCharset       bool                 `yaml:"detect-charset"`
+	DisallowedDomains   []string             `yaml:"disallowed-domains"`
+	IgnoreRobotsTxt     bool                 `yaml:"ignore-robots-txt"`
+	Limit               configCrawlerLimit   `yaml:"limit"`
+	MaxBodySize         int                  `yaml:"max-body-size"`
+	MaxDepth            int                  `yaml:"max-depth"`
+	MaxUrlVisited       int64                `yaml:"max-url-visited"`
+	RandomUserAgents    bool                 `yaml:"random-user-agents"`
+	SleepBetweenRequest time.Duration        `yaml:"sleep-between-request"`
+	Storage             configCrawlerStorage `yaml:"storage"`
+	Proxy               []string             `yaml:"proxy"`
+	UserAgents          []string             `yaml:"user-agents"`
+}
+
+type configCrawlerLimit struct {
+	DomainRegexp string        `yaml:"domain-regexp"`
+	DomainGlob   string        `yaml:"domain-glob"`
+	Delay        time.Duration `yaml:"delay"`
+	RandomDelay  time.Duration `yaml:"random-delay"`
+	Parallelism  int           `yaml:"parallelism"`
+}
+
+type configCrawlerStorage struct {
+	ClearOnStart bool          `yaml:"clear-on-start"`
+	Redis        configRedis   `yaml:"redis"`
+	Mongo        configMongoDb `yaml:"mongo"`
+}
+
+type configMongoDb struct {
+	Database string `yaml:"database"`
+	Uri      string `yaml:"uri"`
+}
+
+type configRedis struct {
+	Address  string `yaml:"address"`
+	Password string `yaml:"password"`
+	Db       int    `yaml:"db"`
+	Prefix   string `yaml:"prefix"`
 }
 
 // Create a Config from a yaml string
 func NewConfigFromString(data string) Config {
 	newConf := Config{}
-
 	err := yaml.Unmarshal([]byte(data), &newConf)
-	util.CheckError(err, "Loading config from a string")
+	//util.CheckError(err, "Loading config from a string")
+	util.FatalError("There is a problem with your configuration", err)
 	return newConf
 }
 
 // Create a Config from yaml file
 func NewConfigFromFile(configFilePath string) Config {
 	if _, err := os.Stat(configFilePath); os.IsNotExist(err) {
-		util.CheckError(err, "Checking existence of a config file")
+		//util.CheckError(err, "Checking existence of a config file")
+		util.FatalError("Cannot find the configuration file", err)
 	}
 
 	configFileContent, err := ioutil.ReadFile(configFilePath)
-	util.CheckError(err, "Loading config file content")
+	//util.CheckError(err, "Loading config file content")
+	util.FatalError("A problem occurred while loading the configuration file", err)
 
 	return NewConfigFromString(string(configFileContent))
 }
